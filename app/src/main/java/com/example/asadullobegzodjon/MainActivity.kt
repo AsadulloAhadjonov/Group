28340b9ff5fd921a1444ad9d653625dbafb5edcd
User <begiakbarov221>
2023-09-23 16:44:10 +0500

    add binding

--- app/src/main/java/com/example/asadullobegzodjon/MainActivity.kt
+++ app/src/main/java/com/example/asadullobegzodjon/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.asadullobegzodjon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.asadullobegzodjon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
+//viewBinding qoshib qoydm ukam
